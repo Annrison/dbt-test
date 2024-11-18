b5dbe27f907052b8810f24dd96fb9efd22dd9673
tai-yee-liou <tai-yee.liou@ibm.com>
2024-11-18 15:26:23 +0800

    [feat](DS) update mertial

--- dbt_jinja/macros/get_methods_simple.sql
+++
@@ -1,3 +0,0 @@
-{% macro get_methods_simple() %}
-    {{ return(["bank_transfer", "credit_card", "gift_card"]) }}
-{% endmacro %}
--- dbt_jinja/macros/get_payment_methods.sql
+++
@@ -1,5 +0,0 @@
-{% macro get_payment_methods() %}
-
-{{ return(get_column_values('payment_method', ref('raw_payments'))) }}
-
-{% endmacro %}
